618eb45d65e49a5272303efec750049cc6c7d4ff
Maciej Manna <maciejmanna@gmail.com>
2019-01-29 23:00:37 +0100

    fixed get_arxiv_data tool; downloaded (1st stage) dataset for gr-qc

--- tools/get_arxiv_data.py
+++ tools/get_arxiv_data.py
@@ -1,16 +1,24 @@
 import time
 import json
 import sys
+import os
 import httplib2
+import datetime
 from lxml import etree
 
-httpClient = httplib2.Http(".cache")
+httpClient = httplib2.Http()
 
 
-def get_query_data(category, offset, numEntries):
+def get_query_data(category, offset, numEntries, year=None):
+    yearPart = ''
+    if year:
+        yearPart = '+AND+submittedDate:'
+        yearPart += '[{}01010000+TO+{}01010000]'.format(year, year + 1)
+
     response, content = httpClient.request(
         'http://export.arxiv.org/api/query' +
         '?search_query=cat:{}'.format(category) +
+        yearPart +
         '&start={}'.format(offset) +
         '&max_results={}'.format(numEntries) +
         '&sortBy=lastUpdatedDate&sortOrder=ascending')
@@ -21,15 +29,36 @@ def get_query_data(category, offset, numEntries):
         return etree.fromstring(content)
 
 
-def get_category_article_count(category):
-    feed = get_query_data(category, 0, 0)
+def get_category_count_and_years(category):
+    feed = get_query_data(category, 0, 1)
     countStr = feed.find('{http://a9.com/-/spec/opensearch/1.1/}totalResults')
+    elem = feed.find('{http://www.w3.org/2005/Atom}entry')
+    publDate = elem.find('{http://www.w3.org/2005/Atom}published').text
+    startYear = int(publDate[:4])
+    endYear = datetime.datetime.now().year
+
+    return (int(countStr.text), startYear, endYear)
+
+
+def get_year_count(category, year):
+    feed = get_query_data(category, 0, 0, year=year)
+    countStr = feed.find('{http://a9.com/-/spec/opensearch/1.1/}totalResults')
+
     return int(countStr.text)
 
 
-def get_parsed_data_chunk(articles, authors, category, offset, numEntries):
-    feed = get_query_data(category, offset, numEntries)
-    for elem in feed.findall('{http://www.w3.org/2005/Atom}entry'):
+def get_parsed_data_chunk(articles, authors, category, offset, numEntries,
+                          year, maxRepeats=10):
+    for repeat in range(0, maxRepeats):
+        feed = get_query_data(category, offset, numEntries, year=year)
+        entries = feed.findall('{http://www.w3.org/2005/Atom}entry')
+        if len(entries) == numEntries:
+            break
+        if (repeat + 1) == maxRepeats:
+            raise BaseException('Failed to get entries after {} trials'
+                                .format(repeat + 1))
+
+    for elem in entries:
         title = elem.find('{http://www.w3.org/2005/Atom}title').text
         publDate = elem.find('{http://www.w3.org/2005/Atom}published').text
         localAuthors = []
@@ -37,29 +66,61 @@ def get_parsed_data_chunk(articles, authors, category, offset, numEntries):
             author = authElem.find('{http://www.w3.org/2005/Atom}name').text
             localAuthors.append(author)
             authors.add(author)
-        articles.append({"title": title,
-                         "published": publDate,
-                         "authors": localAuthors})
+        prevCount = len(articles)
+        newArt = {"title": title,
+                  "published": publDate,
+                  "authors": localAuthors}
+        articles.append(newArt)
+        if prevCount == len(articles):
+            print('APPEND ERROR: {}'.format(newArt))
 
 
-def get_parsed_data_for_category(articles, authors, category, chunkSize=100,
-                                 queryIntervalInSecs=3):
-    totalCount = get_category_article_count(category)
+def print_progress_info(year, totalCount, currentCount):
+    perc = int((currentCount / totalCount) * 100)
+    print(' [{}%] Data for year {}: downloaded {} of {}'
+          .format(perc, year, currentCount, totalCount),
+          end='\r')
+
+
+def get_parsed_data_for_category(authors, category, chunkSize=500,
+                                 queryIntervalInSecs=3, year=None):
+    if year:
+        begYear = year
+        endYear = year
+        totalCount = get_year_count(category, year)
+    else:
+        totalCount, begYear, endYear = get_category_count_and_years(category)
+
     time.sleep(queryIntervalInSecs)
-    totalCount = 35
-    chunkSize = 10
-    print('Attempting to download data on {} articles in category \"{}\".'
-          .format(totalCount, category))
-    for offset in range(0, totalCount, chunkSize):
-        print('Completed: {}/{}'.format(offset, totalCount), end='\r')
-        get_parsed_data_chunk(articles, authors, category, offset, chunkSize)
-        time.sleep(queryIntervalInSecs)
-    print('Completed: {}/{}'.format(totalCount, totalCount))
+    print('Attempting to download data on {} articles in category \"{}\"'
+          .format(totalCount, category) +
+          ' in years {}-{}.'.format(begYear, endYear))
+    articleCount = 0
+
+    for year in range(begYear, endYear + 1):
+        articles = []
+        yearCount = get_year_count(category, year)
+        remaining = yearCount
+        for offset in range(0, yearCount, chunkSize):
+            print_progress_info(year, yearCount, offset)
+            time.sleep(queryIntervalInSecs)
+            currChunkSize = chunkSize
+            if offset + chunkSize > yearCount:
+                currChunkSize = yearCount - offset
+            get_parsed_data_chunk(articles, authors, category, offset,
+                                  currChunkSize, year=year)
+        print('Downloaded article data for year {} ({}/{} entries).'
+              .format(year, len(articles), yearCount),
+              end=' ')
+        filename = '{}_articles_{}.json'.format(category, year)
+        write_articles_data(articles, filename)
+        print('Article data written to file: {}.'.format(filename))
+        articleCount += len(articles)
+    print('Downloaded data on {} articles out of expected {} from {} category'
+          .format(articleCount, totalCount, category))
 
 
 def write_articles_data(articles, filename):
-    print('Writing parsed data of {} articles to: {}.'
-          .format(len(articles), filename))
     with open(filename, 'w', encoding='utf-8') as artFile:
         json.dump(articles, artFile, indent=2)
 
@@ -69,25 +130,42 @@ def write_authors_data(authors, filename):
     with open(filename, 'w', encoding='utf-8') as auFile:
         index = 0
         auFile.write('# {}\n'.format(len(authors)))
+        auFile.write('# author_id;author_name\n'.format(len(authors)))
         for auth in authors:
-            auFile.write('{}\t{}\n'.format(index, auth))
+            auFile.write('{};{}\n'.format(index, auth))
             index += 1
 
 
-def get_arxiv_data(category):
-    authors = set()
-    articles = []
+def preload_authors_set(authors, filename):
+    if not os.path.exists(filename):
+        return
+    print('Preloading current author list from {}.'.format(filename))
+    with open(filename, 'r', encoding='utf-8') as auFile:
+        for line in auFile:
+            if len(line) > 0 and line[0] != '#':
+                authors.add(line.split(';')[1].rstrip())
 
-    get_parsed_data_for_category(articles, authors, 'gr-qc')
-    write_articles_data(articles, '{}_articles.json')
-    write_authors_data(articles, '{}_author_ids.txt')
+
+def get_arxiv_data(category, year=None):
+    authors = set()
+    auFilename = '{}_author_ids.txt'.format(category)
+    if year:
+        preload_authors_set(authors, auFilename)
+    get_parsed_data_for_category(authors, 'gr-qc', year=year)
+    write_authors_data(authors, auFilename)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
         raise BaseException('Invalid command line arguments.')
     else:
         category = sys.argv[1]
+        year = None
+        if len(sys.argv) == 3:
+            year = int(sys.argv[2])
+
         print('Download data from arXiv category \"{}\".'.format(category))
-        get_arxiv_data(category)
+        get_arxiv_data(category, year)
         print('Data successfully downloaded and saved.')
+
+# 2013, 2015, 2016
