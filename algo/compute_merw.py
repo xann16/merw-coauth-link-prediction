ab5f67a228fc711e0f3336245501aee6fb502a22
Piotr <piotr.pikul@student.uj.edu.pl>
2019-02-15 20:16:41 +0100

    SimRank update + small test

--- algo/compute_merw.py
+++ algo/compute_merw.py
@@ -13,14 +13,14 @@ def graph_to_matrix(graph):
 
 
 def compute_merw(A: num.array):
-    w,v = alg.eig(A)
+    w, v = alg.eig(A)
     maxeigi = 0
     for i in range(1, len(w)):
         if w[maxeigi] < w[i]:
             maxeigi = i
     evalue = w[maxeigi]
     evector = v[:, maxeigi]
-    n, = A.shape
+    n, m = A.shape
     P = num.zeros(A.shape)
     for row in range(n):
         denom = evalue * evector[row]
@@ -32,20 +32,23 @@ def compute_merw(A: num.array):
 def compute_merw_simrank(graph, alpha, precision=1e-5, maxiter=100):
     n = len(graph)
     R = num.identity(n)
-    P, v, val, = compute_merw(graph_to_matrix(graph))
+    P, v, val, sdist = compute_merw(graph_to_matrix(graph))
     denom = [[v[x]*v[y] for x in range(n)] for y in range(n)]
     alpha = alpha / val / val
     for iteration in range(maxiter):
         S = num.zeros((n, n))
         for y in range(n):
             for x in range(n):
-                for a in graph[x]:
-                    for b in graph[y]:
-                        S[x][y] += R[a][b] / denom[a][b]
-                S[x][y] *= alpha * denom[x][y]
+                if x == y:
+                    S[x][y] = 1.0
+                else:
+                    for a in graph[x]:
+                        for b in graph[y]:
+                            S[x][y] += R[a][b] / denom[a][b]
+                    S[x][y] *= alpha * denom[x][y]
         eps = alg.norm(R - S)
         if eps < precision:
-            return S, eps
+            return R, eps
         R = S
     return R, eps
 
@@ -57,13 +60,16 @@ def compute_basic_simrank(graph, alpha, precision=1e-5, maxiter=100):
         S = num.zeros((n, n))
         for y in range(n):
             for x in range(n):
-                for a in graph[x]:
-                    for b in graph[y]:
-                        S[x][y] += R[a][b]
-                    S[x][y] /= len(graph[y])
-                S[x][y] *= alpha / len(graph[x])
+                if x == y:
+                    S[x][y] = 1.0
+                else:
+                    for a in graph[x]:
+                        for b in graph[y]:
+                            S[x][y] += R[a][b]
+                    S[x][y] *= alpha / len(graph[x]) / len(graph[y])
         eps = alg.norm(R - S)
         if eps < precision:
-            return S, eps
+            return R, eps
         R = S
     return R, eps
+
---
+++ algo/test.py
@@ -0,0 +1,51 @@
+import compute_merw as merw
+import numpy as num
+
+
+def print_similarity_matrix(S):
+    for row in S:
+        print('[', end='')
+        for value in row:
+            print('{:6.3f} '.format(value), end='')
+        print(']')
+
+
+def print_adiacency_matrix(A):
+    for row in A:
+        print('[', end='')
+        for val in row:
+            if val == 1:
+                print('X ', end='')
+            else:
+                print('. ', end='')
+        print(']')
+
+
+def unit_test(graph):
+    A = merw.graph_to_matrix(graph)
+    print('\n>>>> GRAF: ')
+    print_adiacency_matrix(A)
+    Rsim, eps = merw.compute_basic_simrank(graph, .9, 1e-9, 1000)
+    print('Zwykły SimRank')
+    print_similarity_matrix(Rsim)
+    print('Dokładność:', eps)
+    Rmerw, eps = merw.compute_merw_simrank(graph, 1., 1e-8, 1000)
+    print('MERW SimRank')
+    print_similarity_matrix(Rmerw)
+    print('Dokładność:', eps)
+    print('Samo MERW')
+    P, val, vekt, dist = merw.compute_merw(A)
+    print('Rozkład stacjonarny: ', dist)
+    print_similarity_matrix(P)
+
+
+if __name__ == '__main__':  # Odrobina testów
+    graph1 = [[1, 2], [0, 2], [0, 1, 3, 4], [2, 4],
+              [2, 3, 5], [4, 6], [5, 7, 8], [6, 8],
+              [6, 7, 9, 10], [8,10], [8,9]]
+    graph2 = [[1, 2, 3 ,4, 5, 6, 7, 8, 9, 10],
+              [0, 2], [0, 1, 3], [0, 2, 4], [0, 3, 5],
+              [0, 4, 6], [0, 5, 7], [0, 6, 8], [0, 7, 9],
+              [0, 8, 10], [0, 9]]
+    unit_test(graph1)
+    unit_test(graph2)
